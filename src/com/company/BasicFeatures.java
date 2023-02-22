fca15bdd9cb49f4d87b1ad23d0bc573bf30ccbf6
hamdin <hamdingenc1989@gmail.com>
2023-02-22 15:49:28 -0800

    the basic structure of the car is ready, working on advance features

---
+++ src/com/company/BasicFeatures.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class BasicFeatures {
+
+       String engineCapacity;
+        String color;
+        String doors;
+        String headlights;
+        String regNo;
+
+
+}
