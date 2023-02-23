4585ed40c37f9c75bb9b0dd567108602323092eb
hamdin <hamdingenc1989@gmail.com>
2023-02-22 21:56:09 -0800

    the settings of the car are set to 95MHZ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,6 +12,11 @@ public class Main {
         Audi.engineCapacity="V8";
         Audi.headlights="blue";
         Audi.regNo="tuzik";
+
+        //Let's write our own code.
+        System.out.println("Welcome to my car");
+        System.out.println("the radio is set to 80MHZ");
+        System.out.println("Turn on the air conditioner");
     }
 
 }
